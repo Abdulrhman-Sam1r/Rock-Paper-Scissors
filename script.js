247c7625213e23a5b4652d9e2c74343dbb5edea0
Abdulrhman_Samir <abdulrhman.2.samir@gmail.com>
2025-04-26 13:45:07 +0300

    Create playRound function
    this function takes 2 arguments human choice and computer choice then plays the round and decides the winner

--- script.js
+++ script.js
@@ -3,15 +3,56 @@ function getComputerChoice (){
 
     console.log(num);
     if(num > 0.666)
-        return "Rock";
+        return "rock";
     else if (num > 0.333)
-        return "Paper";
+        return "paper";
     else
-        return "Scissors"
+        return "scissors";
 }
 
 function getHumanChoice (){
-    return prompt("Rock Paper Scissors");
+    return console.prompt("Rock Paper Scissors");
 }
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    console.log("Human score:"+humanScore+"Computer score:"+computerScore)
+    console.log("Round"+round);
+    console.log("Computer Choice:"+computerChoice);
+    console.log("Human Choice:"+humanChoice);
+
+    round++;
+    if(humanChoice == computerChoice)
+        return console.log("Tie!");
+    else if (humanChoice == "rock" &&  computerChoice == "paper"){
+        computerScore++;
+        return console.log("You lose! Paper beats Rock");
+    }
+    else if (humanChoice == "rock" &&  computerChoice == "scissors"){
+        humanScore++;
+        return console.log("You win! Rock beats Scissors");
+    }
+    else if (humanChoice == "paper" &&  computerChoice == "rock"){
+        humanScore++;
+        return console.log("You win! Paper beats Rock");
+    }
+    else if (humanChoice == "paper" &&  computerChoice == "scissors"){
+        computerScore++;
+        return console.log("You lose! Scissors beats Paper");
+    }
+    else if (humanChoice == "scissors" &&  computerChoice == "rock"){
+        computerScore++;
+        return console.log("You lose! Rock beats Scissors");
+    }
+    else if (humanChoice == "scissors" &&  computerChoice == "paper"){
+        humanScore++;
+        return console.log("You win! Scissors beats Paper");
+    }
+}
+
+let round = 1;
+let computerScore = 0;
+let humanScore = 0;
 
 console.log(getHumanChoice());
